8bf7b8e3f99cbe6a4018d9b39f3239913e94d7ae
Chriscommit <christian.piquemil@gmail.com>
2021-05-11 14:17:23 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// **********************************************************************************
+// ********************************* Code Principal *********************************
+// **********************************************************************************
+
+/* création d'une fonction qui va démarrer l'application 
+
+
+*/
+$(function() {
+    // Création puis démarrage de l'application.
+    const magicalSlate = new Program();
+    magicalSlate.start();
+});
+
+
+/*document.addEventListener("DOMContentLoaded", function(){
+	// Création puis démarrage de l'application.
+    const magicalSlate = new Program();
+    magicalSlate.start();
+});*/
